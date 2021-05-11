1a05ca6d5398ede84bcd8a05a57f245fa003ab39
jackson balest <balestjackson@gmail.com>
2021-05-10 22:11:26 -0300

    Adicionei meu comandos basicos em c#

---
+++ course/course/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace course {
+    class Program {
+        static void Main(string[] args) {
+            //tipos de variaveis
+            bool completo = false;
+            char genero = 'F';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "maria";
+            
+            //todas os tipos de variaveis vem da classe object
+            object obj1 = "joao";
+            object obj2 = 4.5f;
+
+            //imprimindo na tela as variaveis
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+            //Pergar o valor minimo e maximo de cada tipo numerico
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+        }
+    }
+}
---
+++ saidaDeDados/saidaDeDados/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace saidaDeDados {
+    class Program {
+        static void Main(string[] args) {
+            
+            //saida comum
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("boa noite!");
+            Console.WriteLine("<------->");
+
+            //Saida de variaveis
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 100.578941;
+            string nome = "Maria";
+
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+
+            //Placeholders, concatenção e interpolação
+            Console.WriteLine("<------------>");
+            Console.WriteLine("{0} tem {1} anos de e tem slado igual a {2:F2}", nome, idade, saldo);
+
+            Console.WriteLine("<------------>");
+            Console.WriteLine(nome +" Tem "+ idade +", de genero "+ genero + " Com salso de "+ saldo.ToString("F2"));
+
+            Console.WriteLine("<------------>");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo.ToString("F2")} R$");
+        }
+    }
+}
