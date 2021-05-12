601019846043b93ad94a46a4e2fff50a8d894d4d
jackson balest <balestjackson@gmail.com>
2021-05-11 23:10:09 -0300

    conversao implicita e de casting

---
+++ convercaoImplicitaCasting/convercaoImplicitaCasting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace convercaoImplicitaCasting {
+    class Program {
+        static void Main(string[] args) {
+            
+            
+            float x = 4.5f;
+            double y = x;
+
+            double a = 4.5;
+            float b = (float)a;
+
+            int c = (int)b;
+
+            int ex1 = 5;
+            int ex2 = 2;
+            double ex3 = ex1 / ex2;
+
+        }
+    }
+}
