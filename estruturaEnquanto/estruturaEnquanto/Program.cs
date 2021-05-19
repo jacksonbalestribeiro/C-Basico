f5bd2f47ffe845b89c6361268a854cb9c282cd8d
jackson balest <balestjackson@gmail.com>
2021-05-18 22:35:59 -0300

    ADD loop while

---
+++ estruturaEnquanto/estruturaEnquanto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace estruturaEnquanto {
+    class Program {
+        static void Main(string[] args) {
+
+            double numero = 1;
+            double resultado = 0;
+            while (numero > 0) {
+                Console.WriteLine("Escreva o numero que voce quer a raiz quadrada:");
+                numero = double.Parse(Console.ReadLine());
+                if (numero < 0) {
+                    Console.WriteLine("numero {0} negativo", numero);
+                } else {
+                    resultado = Math.Sqrt(numero);
+                    Console.WriteLine("Raiz quadrada de {0} = {1} ", numero, resultado.ToString("F3"));
+                }
+            }
+
+
+        }
+    }
+}
