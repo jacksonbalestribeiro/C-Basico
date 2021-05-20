cbd0dd2481399d19ac13ca39eb61dd65c8168b53
jackson balest <balestjackson@gmail.com>
2021-05-19 21:16:49 -0300

    ADD loop FOR

---
+++ estruturaFor/estruturaFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace estruturaFor {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Quantos numeros inteiros voce quer digitar?");
+            int qtd = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 0; i < qtd; i++) {
+                Console.Write($"Valor #{i+1}:");
+                soma += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
