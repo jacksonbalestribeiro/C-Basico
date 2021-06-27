ddbb05363355d1366bb9411c1f2bfe7f5fcc931f
jackson balest <balestjackson@gmail.com>
2021-06-27 16:00:49 -0300

    Atualizando!

--- Operador_Tenario/Operador_Tenario/Program.cs
+++ Operador_Tenario/Operador_Tenario/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Globalization;
+
 
 namespace Operador_Tenario {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine($"Preço final: {preco - desconto}");
+
         }
     }
 }
